c5c3921fb4ac6bb4b12113fc8b30524f763465cf
Aidan Smith <asmith2@scu.edu>
2022-08-23 17:40:28 -0700

    New fetch

--- netlify/functions/song/song.js
+++ netlify/functions/song/song.js
@@ -1,35 +1,28 @@
 import fetch from 'node-fetch';
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-var show_title
-var current_show
 const handler = async (event) => {
   try {
     const subject = event.queryStringParameters.name || 'World'
     const url = `https://spinitron.com/api/`
     const access_string = `?access-token=`
     const key = `fsr9w2R8irUUqUkze_QUcyB3`
-
     show_url = url + `shows/` + access_string + key + `&count=1`
-    fetch(show_url)
-      .then((response) => response.json())
-      .then((data) => {
+    let response = await fetch(show_url);
+    let data = await response.json();
+    console.log("Recieved data");
+    items = data["items"][0]
+    console.log(items['title'])
+    show_title = items['title']
 
-        console.log("Recieved data");
-        items = data["items"][0]
-        console.log(items['title'])
-        show_title = items['title']
-
-        // Testing dates
-        let start_time = new Date(items['start'])
-        const current_time = new Date()
-        console.log(start_time < current_time)
-        if (start_time < current_time) {
-          current_show = true
-        } else {
-          current_show = false
-        }
-      }
-      );
+    // Testing dates
+    let start_time = new Date(items['start'])
+    const current_time = new Date()
+    console.log(start_time < current_time)
+    if (start_time < current_time) {
+      current_show = true
+    } else {
+      current_show = false
+    }
     // let name = alert(data["items"])
     // console.log
 
