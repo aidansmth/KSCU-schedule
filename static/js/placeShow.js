0b210818c67f8866ea839e31dfe5ba275872d80d
Aidan Smith <asmith2@scu.edu>
2022-09-02 00:10:15 -0700

    Added caching Added track to header

--- static/js/placeShow.js
+++ static/js/placeShow.js
@@ -10,75 +10,67 @@ const formatAMPM = (date) => {
     return strTime;
 }
 
-async function getShow() {
-    title = localStorage.getItem('title')
-    if (title !== null) {
-        start_time = new Date(localStorage.getItem('start_time'))
-        end_time = new Date(localStorage.getItem('end_time'))
-        DJ_name = localStorage.getItem('DJ_name')
-        console.log("Found show data in local memory...")
-        if (start_time > new Date()) {
-            console.log("Show is upcoming...")
-            document.getElementById("title").innerHTML = "Next Show: " + title;
-            document.getElementById("DJ").innerHTML += " with " + DJ_name + " at " + formatAMPM(start_time) + "\n"
-        } else {
-            if (end_time < new Date()) { // Test to make sure show is still live
-                // Throw error
-                throw new Error("Show is in the past...")
-            }
-            console.log("Show is currently playing...")
-            document.getElementById("title").innerHTML = "Live now: " + title;
-            document.getElementById("DJ").innerHTML += " with " + DJ_name + " till " + formatAMPM(end_time) + "\n"
-        }
-        return
-    }
-    console.log("Entered function...")
+async function fetchShow() {
     try {
         request = `/.netlify/functions/show`
         console.log("Request: " + request)
         let response = await fetch(request);
         let data = await response.json();
         console.log("Recieved data...");
-        console.log(data)
-        let title = data['title']
-        console.log("Show title: " + title)
-        category = data['category']
-        start_time = new Date(data['start_time'])
-        end_time = new Date(data['end_time'])
-        duration = data['duration']
-        DJ_name = data['DJ_name']
-        DJ_bio = data['DJ_bio']
-        DJ_since = data['DJ_since']
-
-        localStorage.setItem('title', title)
-        localStorage.setItem('category', category)
-        localStorage.setItem('start_time', start_time)
-        localStorage.setItem('end_time', end_time)
-        localStorage.setItem('duration', duration)
-        localStorage.setItem('DJ_name', DJ_name)
-        localStorage.setItem('DJ_bio', DJ_bio)
-        localStorage.setItem('DJ_since', DJ_since)
-        console.log("Set local storage...")
-
-        // Test if the show is currently playing or upcoming
-        if (start_time > new Date()) {
-            console.log("Show is upcoming...")
-            document.getElementById("title").innerHTML = "Next Show: " + title;
-            document.getElementById("DJ").innerHTML += " with " + DJ_name + " at " + formatAMPM(start_time) + "\n"
-        } else {
-            if (end_time < new Date()) { // Test to make sure show is still live
-                // Throw error
-                throw new Error("Show is in the past...")
-            }
-            console.log("Show is currently playing...")
-            document.getElementById("title").innerHTML = "Live now: " + title;
-            document.getElementById("DJ").innerHTML += " with " + DJ_name + " till " + formatAMPM(end_time) + "\n"
-        }
+        store('showData', data)
         return { statusCode: 500, body: "Next Show: " + title }
     }
     catch (error) {
         return { statusCode: 500, body: error.toString() }
     }
+}
+
+async function getShow() {
+
+    function placeData(data) {
+        // if show is upcoming
+        if (new Date(data['start_time']) > Date.now()) {
+            // if show is in the future
+            document.getElementById('isLive').innerHTML = "Up Next on KSCU:"
+            document.getElementById('title').innerHTML = data['title']
+            document.getElementById('DJ').innerHTML = data['DJ_name'] + " at " + formatAMPM(new Date(data['start_time']))
+
+        } else {
+            // if show is live
+            document.getElementById('isLive').innerHTML = "Live Now:  ";
+            console.log("Starting animation.")
+            document.getElementById('live-now-circle').style.animationPlayState = "running";
+            document.getElementById('live-now-circle').style.display = "inline-block";
+            document.getElementById("title").innerHTML = data['title'];
+            document.getElementById("DJ").innerHTML = "with " + data['DJ_name'] + " till " + formatAMPM(new Date(data['end_time']));
+        }
+    }
+
+    if (store.has("showData")) {
+        data = store.get("showData")
+        end_time = new Date(data['end_time'])
+        if (end_time > new Date()) {
+            // Show is currently on
+            if ((data['title']) != document.getElementById("title").innerHTML) {
+                console.log("Old show is still on air, placing data...")
+                placeData(data)
+            } else {
+                console.log("Show in DOM does not match current show, fetching new show...")
+                x = await fetchShow()
+                placeData(store.get("showData"))
+            }
+        } else {
+            console.log("Show is over, fetching new show...")
+            x = await fetchShow()
+            placeData(store.get("showData"))
+        }
+    } else {
+        console.log("No show data found, fetching...")
+        x = await fetchShow()
+        placeData(store.get("showData"))
+    }
+    // Repeat after 5 minutes
+    setTimeout(getShow, 300000)
 }
 
 getShow()
--- static/js/recentTrack.js
+++ static/js/recentTrack.js
@@ -1,4 +1,35 @@
 
+function insertTracks(data) {
+    function duration(duration_time) {
+        let minutes = Math.floor(duration_time / 60)
+        let seconds = duration_time % 60
+        if (seconds < 10) {
+            seconds = "0" + seconds
+        }
+        return minutes + ":" + seconds
+    }
+
+    // Function to add quotes to a string
+
+    first_duration = data[0]["duration"]
+    document.getElementById("current-track").innerHTML = data[0]["song"]
+    document.getElementById("current-artist").innerHTML = data[0]["artist"]
+    image = String(data[0]['image'])
+    console.log(image)
+    let imageSRC = document.getElementById("album-art")
+    imageSRC.src = image
+    let list = document.getElementById("past-tracks");
+    // Clear the list
+    list.innerHTML = ""
+    for (let i = 1; i < data.length; i++) {
+        let row = document.createElement("tr");
+        song_title = data[i]["song"]
+        artist = data[i]["artist"]
+
+        row.innerHTML = song_title + " (" + duration(data[i]["duration"]) + ")";
+        list.appendChild(row);
+    }
+}
 
 function trimToLength(string, length) {
     if (string.length > length) {
@@ -8,36 +39,29 @@ function trimToLength(string, length) {
     }
 }
 
-async function putRecentTracks() {
-    function duration(duration_time) {
-        let minutes = Math.floor(duration_time / 60)
-        let seconds = duration_time % 60
-        if (seconds < 10) {
-            seconds = "0" + seconds
-        }
-        return minutes + ":" + seconds
-    }
+async function fetchTracks() {
+    console.log("Fetching track data...");
     request = `/.netlify/functions/spins`
     console.log("Request: " + request)
     let response = await fetch(request);
     let data = await response.json();
     console.log("Recieved track data...");
-    console.log(data)
-    console.log(typeof data)
-    document.getElementById("current-track").innerHTML = "Currently playing:\n"
-    first_duration = data[0]["duration"]
-    document.getElementById("current-track").innerHTML += data[0]["song"] + " by " + data[0]["artist"] + " (" + duration(first_duration) + ")\n"
-    document.getElementById("past-tracks").innerHTML = "Past tracks:"
-    let list = document.getElementById("tracks-list");
-    for (let i = 1; i < data.length; i++) {
-        let row = document.createElement("tr");
-        song_title = data[i]["song"]
-        artist = data[i]["artist"]
+    console.log(data);
 
-        row.innerHTML = song_title + " by " + artist + " (" + duration(data[i]["duration"]) + ")";
-        list.appendChild(row);
+    insertTracks(data)
+    store("recentTracks", data)
+    console.log("Stored track data...");
+    insertTracks(data)
+    setTimeout(fetchTracks, 15000)
+}
+
+
+async function putRecentTracks() {
+    if (store.has("recentTracks")) {
+        insertTracks(store.get("recentTracks"));
+        console.log("Inserted stored track data...");
     }
-
+    fetchTracks();
 }
 
 putRecentTracks()
